d2469e274cb4389bb6d81f63782e5560dfa6f9bf
FaithAchieng <faithachienglexus@gmail.com>
2024-11-25 14:54:00 +0300

    added correct path for data

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const monthly = document.getElementById('Monthly');
     const cards = document.querySelectorAll('.work, .social, .play, .exercise, .self, .study');
 
-    fetch("/data.json")
+    fetch("./data.json")
         .then(response => {
             if (!response.ok) {
                 throw new Error("Could not fetch the data");
